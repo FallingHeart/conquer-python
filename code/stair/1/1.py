9607efb3b23d29328f53081e55b5b38f1dfc108e
rex nathan <hi@dat-day.com>
2020-07-20 16:45:37 +0800

    update

--- code/stair/1/1.py
+++ code/stair/1/1.py
@@ -9,3 +9,15 @@ print(some_dict['name'][1])
 some_dict = some_data_frame.to_dict('list')
 print(some_dict)
 print(some_dict['name'][1])
+
+
+import requests
+some_result = requests.get('https://api.bamo.tech/v0/cosmetic/mostUsedCF?top=5')
+print(some_result)
+
+some_json = some_result.json()
+print(some_json)
+
+some_list = some_json['list']
+print(some_list)
+print(some_list[0])
